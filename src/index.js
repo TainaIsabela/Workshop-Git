fb258b9f03873673dee29403c28d340e35189b15
Tainá Isabela <30873852+TainaIsabela@users.noreply.github.com>
2023-05-06 18:37:36 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Confetti from "react-confetti";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>1º Fase Concluída</h1>
+      <Confetti />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
